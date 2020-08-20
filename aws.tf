0f73c7514ef8707808e29508daecaddcc18ebc75
William Patton <wjpatton@wjpatton.com>
2020-08-20 12:07:16 -0500

    Update aws.tf

--- aws.tf
+++ aws.tf
@@ -29,33 +29,3 @@ resource "aws_route53_record" "aws_sub_zone_ns" {
     awsns
   ]
 }
-
-
-# GCP SubZone
-
-resource "aws_route53_zone" "gcp_sub_zone" {
-  count         = var.create_gcp_dns_zone ? 1 : 0
-  name          = "${var.namespace}.gcp.${var.hosted-zone}"
-  comment       = "Managed by Terraform, Delegated Sub Zone for GCP for  ${var.namespace}"
-  force_destroy = false
-
-  tags = {
-    name       = var.namespace
-    owner      = var.owner
-    created-by = var.created-by
-    ttl        = "-1"
-  }
-}
-
-resource "aws_route53_record" "gcp_sub_zone" {
-  count   = var.create_gcp_dns_zone ? 1 : 0
-  zone_id = data.aws_route53_zone.main.zone_id
-  name    = "${var.namespace}.gcp.${var.hosted-zone}"
-  type    = "NS"
-  ttl     = "30"
-
-  records = [
-    for gcpns in google_dns_managed_zone.gcp_sub_zone.0.name_servers :
-    gcpns
-  ]
-}
