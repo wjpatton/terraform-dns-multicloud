f044acf922610072ea14759381452299ec510dc9
William Patton <wjpatton@wjpatton.com>
2020-08-20 12:06:16 -0500

    Update aws.tf

--- aws.tf
+++ aws.tf
@@ -30,33 +30,6 @@ resource "aws_route53_record" "aws_sub_zone_ns" {
   ]
 }
 
-# Azure SUBZONE
-
-resource "aws_route53_zone" "azure_sub_zone" {
-  count   = var.create_azure_dns_zone ? 1 : 0
-  name    = "${var.namespace}.azure.${var.hosted-zone}"
-  comment = "Managed by Terraform, Delegated Sub Zone for Azure for ${var.namespace}"
-
-  tags = {
-    name       = var.namespace
-    owner      = var.owner
-    created-by = var.created-by
-    ttl        = "-1"
-  }
-}
-
-resource "aws_route53_record" "azure_sub_zone_ns" {
-  count   = var.create_azure_dns_zone ? 1 : 0
-  zone_id = data.aws_route53_zone.main.zone_id
-  name    = "${var.namespace}.azure.${var.hosted-zone}"
-  type    = "NS"
-  ttl     = "30"
-
-  records = [
-    for azurens in azurerm_dns_zone.azure_sub_zone.0.name_servers :
-    azurens
-  ]
-}
 
 # GCP SubZone
 
