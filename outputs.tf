d41e1481b1fe8cdab789236e9209107a0aa29f36
William Patton <wjpatton@wjpatton.com>
2020-08-20 12:07:31 -0500

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -5,11 +5,3 @@ output "aws_sub_zone_id" {
 output "aws_sub_zone_nameservers" {
   value = var.create_aws_dns_zone ? aws_route53_zone.aws_sub_zone[0].name_servers : []
 }
-
-output "gcp_dns_zone_name" {
-  value = var.create_gcp_dns_zone ? google_dns_managed_zone.gcp_sub_zone[0].name : ""
-}
-
-output "gcp_dns_zone_nameservers" {
-  value = var.create_gcp_dns_zone ? google_dns_managed_zone.gcp_sub_zone[0].name_servers : []
-}
